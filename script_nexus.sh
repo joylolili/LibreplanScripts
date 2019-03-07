6bb88ff8e415fbfb0c606929b28dda4a7915728e
joylolili <jwoynglin@outlook.fr>
2019-03-07 18:29:31 +0100

    modifier script nexus

--- script_nexus.sh
+++ script_nexus.sh
@@ -4,7 +4,9 @@
 
 
 cd /home/tomcat/.jenkins/workspace/ConstructionWARLibreplan/libreplan-webapp/target
+OLD_FILE="libreplan-webapp.war"
 export WAR_FILE="libreplan-webapp.war"
+mv ${OLD_FILE} ${WAR_FILE} 
 curl --fail -u admin:admin123 --upload-file ${WAR_FILE} ${IP_NEXUS}
 RET=$?
 if [ ${RET} -eq 0 ]
