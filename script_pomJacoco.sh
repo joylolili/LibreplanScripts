17444917b07340950aa31de24211a6915fedd260
joylolili <jwoynglin@outlook.fr>
2019-03-07 18:59:37 +0100

    modifier script nexus

--- script_pomJacoco.sh
+++ script_pomJacoco.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 #Changer le pom.xml pour sonde JaCoCo
+LIBREPLAN="/home/tomcat/.jenkins/workspace/ConstructionWARLibreplan"
+SCRIPTSDossier="/home/tomcat/.jenkins/workspace/scripts"
 
-$SCRIPTS/pom.xml
+cd ${LIBREPLAN}
+[ -d pom.xml ] && rm -rf pom.xml
+cd ${SCRIPTSDossier}
+cp pom.xml ${LIBREPLAN}/pom.xml
+echo "pom.xml a été modifié"
 
-$LIBREPLAN/
